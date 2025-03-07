bc50529e65f287c194dfde35abfd2f50cbc55541
Suresh Shrestha <sureshcstha@gmail.com>
2025-03-07 14:08:17 -0700

    clean up code

--- routes/apiAuthRoutes.js
+++ routes/apiAuthRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const { signup, changePassword, regenerateClientSecret, accessToken, deleteApiUser } = require('../controllers/apiUserController');
 
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const router = express.Router();
-
 
 router.post('/signup', signup);
 router.put("/change-password", changePassword);
