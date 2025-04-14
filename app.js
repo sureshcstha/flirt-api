6b6241e4582f545905bb98bff2017b6aebb4b9d6
Suresh Shrestha <sureshcstha@gmail.com>
2025-04-14 16:54:12 -0600

    make PATCH allowed for cross-origin requests

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cors({
         }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
